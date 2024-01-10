319e492c3a69d300825c5e88c7a4ec655cc854a0
kcsandyy <kemboisandra20@gmail.com>
2024-01-10 08:55:56 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def replace(my_list, search, replace):
-    new_list = [item if item == search else replace  for item in my_list]
-    return new_list
+def search_replace(my_list, search, replace):
+    return [replace if item == search else item for item in my_list]
