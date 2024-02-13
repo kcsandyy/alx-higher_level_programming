674545b09b7b3e8a4f2abb27f8d1c6e06b357608
kcsandyy <kemboisandra20@gmail.com>
2024-02-13 11:24:46 -0800

    README.md

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-
 class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
     def __init__(self, size=0):
+        """
+        initialization function for our square clasee
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,4 +17,7 @@ class Square:
             self.__size = size
 
     def area(self):
+        """
+        calculates the area of the square
+        """
         return self.__size ** 2
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
-
 class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
     def __init__(self, size=0):
+        """
+        initialization function for our square clasee
+        """
         if self.__validate_size(size):
             self.__size = size
 
     @property
     def size(self):
+        """
+        getter for the size property
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        setter for the size property
+        """
         if self.__validate_size(value):
             self.__size = value
 
     def area(self):
+        """
+        calculates the area of the square
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        prints the square using '#' characters
+        """
         i = 0
         for i in range(0, self.__size):
             j = 0
@@ -26,6 +45,9 @@ class Square:
             print()
 
     def __validate_size(self, size):
+        """
+        validates the size, checking for errors
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
