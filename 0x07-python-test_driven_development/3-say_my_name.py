019fa3b8afed348ddec1005fe83c673f7a328c5d
kcsandyy <kemboisandra20@gmail.com>
2024-02-13 20:20:38 -0800

    Test driven development

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module say_my_name prints a name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print Full Nname for a person.
+    """
+    if type(first_name) != str:
+        raise TypeError('First_name must be a string')
+    if type(last_name) != str:
+        raise TypeError('Last_name must be a string')
+    print("My name is " + first_name + " " + last_name)
